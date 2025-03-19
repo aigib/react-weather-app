309c7e8641e450553369a919ee519c83f074280e
aigib <aigibuht@gmail.com>
2025-03-19 11:57:40 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App </h1>
-      <footer>
-        This project was coded by Aigi Buht and it is
-        <a
-          href="https://github.com/aigib/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+
+      <div className="container">
+        <footer>
+          This project was coded by Aigi Buht and it is
+          <a
+            href="https://github.com/aigib/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
