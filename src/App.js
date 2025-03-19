286fa72204fa7c1b6fc34ddb38e10c27097d8938
aigib <aigibuht@gmail.com>
2025-03-19 13:02:50 +0200

    Added weather information

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App </h1>
 
+      <Weather />
+
       <div className="container">
         <footer>
-          This project was coded by Aigi Buht and it is
+          This project was coded by Aigi Buht and it is{" "}
           <a
             href="https://github.com/aigib/react-weather-app"
             target="_blank"
