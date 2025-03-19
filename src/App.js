7effe0031201a2a73daeffdd6087594ce99ef4fa
aigib <aigibuht@gmail.com>
2025-03-19 11:03:19 +0200

    Updated the link tag

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ export default function App() {
       <h1>Weather App </h1>
       <footer>
         This project was coded by Aigi Buht and it is
-        <a href="https://github.com/aigib/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/aigib/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>
       </footer>
