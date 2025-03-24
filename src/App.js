64357bcca20a903a5728418c3bbfe5ea01b05a3f
aigib <aigibuht@gmail.com>
2025-03-24 14:34:08 +0200

    Added links to Netlify and personal GitHub

--- src/App.js
+++ src/App.js
@@ -10,13 +10,26 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="New York" />
         <footer>
-          This project was coded by Aigi Buht and it is{" "}
+          This project was coded by{" "}
+          <a href="https://github.com/aigib" target="_blank" rel="noreferrer">
+            Aigi Buht
+          </a>{" "}
+          and it is{" "}
           <a
             href="https://github.com/aigib/react-weather-app"
             target="_blank"
             rel="noreferrer"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://gleeful-horse-3ad954.netlify.app/?"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify
           </a>
         </footer>
       </div>
