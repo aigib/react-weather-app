2f210ac7852b05e1218ab927a22f05e7cee1f762
aigib <aigibuht@gmail.com>
2025-03-31 11:56:38 +0300

    Removed the fahrenheit temperature

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -27,7 +27,7 @@ export default function WeatherForecastDay(props) {
       <WeatherIcon code={props.data.condition.icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
-          {maxTemperature()}
+          {maxTemperature()}{" "}
         </span>
         <span className="WeatherForecast-temperature-min">
           {minTemperature()}
